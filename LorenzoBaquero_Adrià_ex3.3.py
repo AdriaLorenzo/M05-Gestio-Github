6975d188a589a9ef0eb6ccc955a68904fa45d082
AdriaLorenzo <118479185+AdriaLorenzo@users.noreply.github.com>
2022-11-18 13:31:22 +0100

    Delete LorenzoBaquero_Adrià_ex3.3.py

--- LorenzoBaquero_Adrià_ex3.3.py
+++
@@ -1,6 +0,0 @@
-edadperro = int( input('Ingrese la edad de tu perro = ') )
-
-if (edadperro <= 2):
-    print ('La edad de su perro es de = ' , edadperro*10.5)
-else:
-    print ('La edad de su perro es de = ' , (2*10.5) + (edadperro-2)*4)
