2fedd7e81f8a88b0744de64227449162d2c77db4
AdriaLorenzo <118479185+AdriaLorenzo@users.noreply.github.com>
2022-11-18 13:31:37 +0100

    Delete LorenzoBaquero_Adrià_ex3.5.py

--- LorenzoBaquero_Adrià_ex3.5.py
+++
@@ -1,8 +0,0 @@
-numero = int(input('Ingrese un numero = '))
-
-if (numero > 0):
-    print ('Su numero es positivo')
-elif numero == 0:
-    print ('Su numero es 0')
-else:
-    print ('Su numero es negativo')
