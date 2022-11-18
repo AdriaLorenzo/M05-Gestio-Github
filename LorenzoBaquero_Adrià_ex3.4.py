8a870a2c3c47050934dfc129438352834df859db
AdriaLorenzo <118479185+AdriaLorenzo@users.noreply.github.com>
2022-11-18 13:31:30 +0100

    Delete LorenzoBaquero_Adrià_ex3.4.py

--- LorenzoBaquero_Adrià_ex3.4.py
+++
@@ -1,9 +0,0 @@
-contra = input('Ingrese su contraseña = ')
-
-correcto = 'iloveyou123'
-
-
-if (contra == correcto ):
-    print('Enhorabuena! Su contraseña es correcta!')
-else:
-    print('Lo la contraseña insertada es incorrecta!')
