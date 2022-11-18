283f48e56adb758495e529d8c18aeab291456130
AdriaLorenzo <118479185+AdriaLorenzo@users.noreply.github.com>
2022-11-18 13:29:36 +0100

    Add files via upload

---
+++ LorenzoBaquero_Adrià_ex2.2.py
@@ -0,0 +1,7 @@
+nota1 = int( input('Inserta el primer numero = ') )
+nota2 = int( input('Inserta el segundo numero = ') )
+nota3 = int( input('Inserta el tercer numero = ') )
+
+media = ((nota1+nota2+nota3)/3)
+
+print ('La media de estos 3 numeros es' , media)
---
+++ LorenzoBaquero_Adrià_ex3.4.py
@@ -0,0 +1,9 @@
+contra = input('Ingrese su contraseña = ')
+
+correcto = 'iloveyou123'
+
+
+if (contra == correcto ):
+    print('Enhorabuena! Su contraseña es correcta!')
+else:
+    print('Lo la contraseña insertada es incorrecta!')
